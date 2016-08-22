9a76f8018b486d6b98d8cc8c187adbe2775554d1
Mikhail Tukai <tukaym@gmail.com>
2016-08-22 17:43:02 +0300

    добавил events-slider

--- src/js/common.js
+++ src/js/common.js
@@ -33,6 +33,29 @@ $(document).ready(function() {
     autoplay: true
   });
 
+  $('.events-slider .posts').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    autoplay: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
 //maginific popup
   $('.event-gallery__item').magnificPopup({
     type: 'image',
