252ca764738a81dc86f83765b1140ca7cfc19581
Mikhail Tukai <tukaym@gmail.com>
2016-08-26 13:31:23 +0300

    добавил поиск в мобильное меню

--- src/js/common.js
+++ src/js/common.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
   $('.menu-icon').click(function() {
     $('.menu').slideToggle();
   });
+// open search
+  $('.search-icon').click(function() {
+    $('.menu__search').slideToggle('slow');
+  });
 
 // slider
   $('.slider').slick({
