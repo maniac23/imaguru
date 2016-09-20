516a484243223d1c3ea6ddd1b309e54a639fb909
Mikhail Tukai <tukaym@gmail.com>
2016-09-20 10:02:15 +0300

    added senior course block + fixed script

--- src/js/partials/tabs.js
+++ src/js/partials/tabs.js
@@ -1,8 +1,20 @@
-$('.course-tab__links').click(function() {
+// junior
+$('#junior .course-tab__links').click(function() {
   var tab_id = $(this).attr('data-tab');
 
-  $('.course-tab__links').removeClass('course-tab__links--active');
-  $('.course').removeClass('course--is-active');
+  $('#junior .course-tab__links').removeClass('course-tab__links--active');
+  $('.course--junior').removeClass('course--is-active');
+
+  $(this).addClass('course-tab__links--active');
+  $('#' + tab_id).addClass('course--is-active');
+});
+
+// senior
+$('#senior .course-tab__links').click(function() {
+  var tab_id = $(this).attr('data-tab');
+
+  $('#senior .course-tab__links').removeClass('course-tab__links--active');
+  $('.course--senior').removeClass('course--is-active');
 
   $(this).addClass('course-tab__links--active');
   $('#' + tab_id).addClass('course--is-active');
