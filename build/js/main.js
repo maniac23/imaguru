fc6f7ac482cc05e09044f3824f3af343125cedc5
Mikhail Tukai <tukaym@gmail.com>
2016-10-04 17:26:32 +0300

    deleted unnecessary files

--- build/js/main.js
+++ build/js/main.js
@@ -13613,30 +13613,5 @@ $(document).ready(function() {
    	});
    
    }(jQuery));
-   // ;(function($){
-   
-   // 	// promo-carousel
-   // 	var features = $('.promo__main__features-carousel__list');
-   // 	var text = $('.promo__main__features-text');
-   
-   // 	features.slick({
-   // 		slidesToShow: 3,
-   // 		slidesToScroll: 1,
-   // 		asNavFor: '.promo__main__features-text',
-   // 		dots: true,
-   // 		centerMode: true,
-   // 		focusOnSelect: true
-   // 	});
-   
-   // 	text.slick({
-   // 		slidesToShow: 1,
-   // 		slidesToScroll: 1,
-   // 		arrows: false,
-   // 		fade: true,
-   // 		asNavFor: '.promo__main__features-carousel__list'
-   // 	});
-   
-   
-   // }(jQuery));
 
 });
