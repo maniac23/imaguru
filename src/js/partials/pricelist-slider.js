7892d0875e926b45e5ad693d4f799b588ad0f06c
Mikhail Tukai <tukaym@gmail.com>
2016-10-28 15:24:10 +0300

    changed styles & script for pricelist on coworking page

--- src/js/partials/pricelist-slider.js
+++ src/js/partials/pricelist-slider.js
@@ -1,35 +1,41 @@
-$('.pricelist > ul').slick({
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  vertical: false,
-  dots: false,
-  arrows: false,
-  draggable: false,
-  infinite: false,
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 3,
-        arrows: true,
-        draggable: true
+if ($('.pricelist > ul') && $('.pricelist > ul li').length > 2) {
+  $('.pricelist > ul').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    vertical: false,
+    dots: false,
+    arrows: false,
+    draggable: false,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: 'unslick'
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          arrows: true,
+          draggable: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          arrows: true,
+          draggable: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          arrows: true,
+          draggable: true
+        }
       }
-    },
-    {
-      breakpoint: 800,
-      settings: {
-        slidesToShow: 2,
-        arrows: true,
-        draggable: true
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        arrows: true,
-        draggable: true
-      }
-    }
-  ]
-});
+    ]
+  });
+}
