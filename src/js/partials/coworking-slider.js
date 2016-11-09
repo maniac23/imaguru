aa7a680b3aa83456338fdd228fd56318d6c7c1be
Mikhail Tukai <tukaym@gmail.com>
2016-11-09 18:12:54 +0300

    fixed styles

--- src/js/partials/coworking-slider.js
+++ src/js/partials/coworking-slider.js
@@ -1,12 +1,11 @@
-$('#eventsTimetable, #events-slider').slick({
-  slidesToShow: 7,
+$('#eventsTimetable').slick({
+  slidesToShow: 5,
   slidesToScroll: 1,
   vertical: true,
   dots: false,
   arrows: true,
   draggable: true,
   infinite: false,
-  adaptiveHeight: true,
   responsive: [
     {
       breakpoint: 800,
@@ -24,3 +23,27 @@ $('#eventsTimetable, #events-slider').slick({
 });
 
 $('.events-timetable__list .events-timetable__item').equalHeights();
+
+$('#events-slider').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  vertical: true,
+  dots: false,
+  arrows: true,
+  draggable: true,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 2
+      }
+    }
+  ]
+});
