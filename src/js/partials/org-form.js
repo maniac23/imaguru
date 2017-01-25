d166f25e722836edc18ed43a9b08478adcda87a6
Mikhail Tukai <mikhail@imaguru.by>
2017-01-25 14:39:18 +0300

    ugraded register form

--- src/js/partials/org-form.js
+++ src/js/partials/org-form.js
@@ -1,7 +1,9 @@
 $('.maintain-paid li').on('click', function() {
 var atribute = $(this).attr('data-name');
 var checkBox = $("input[name='"+ atribute + "'");
+var label = $('label[for="'+ atribute +'"]');
 $(this).toggleClass('checked');
+label.toggleClass('checked');
 checkBox.attr("checked", !checkBox.attr("checked"));
 });
 
@@ -10,7 +12,23 @@ $('.catering').on('click', function(e) {
   e.preventDefault();
   var atribute = $(this).attr('data-order');
   var checkBox = $("input[name='"+ atribute + "'");
+  var label = $('label[for="'+ atribute +'"]');
   $(this).toggleClass('active');
+  label.toggleClass('checked');
   checkBox.attr("checked", !checkBox.attr("checked"));
 
-})
+});
+
+
+$('input[type="checkbox"]').on('click', function() {
+  var atribute = $(this).attr('id');
+  var label = $('label[for="'+ atribute +'"]');
+  var element = $('.maintain-paid li[data-name="'+ atribute +'"]');
+  label.toggleClass('checked');
+
+  if($(this).is(':checked')) {
+    element.addClass('checked');
+  } else {
+     element.removeClass('checked');
+  }
+});
