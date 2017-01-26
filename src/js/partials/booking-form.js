54f15ce5018c5885b7eb2fd0084b581ed6b30cf1
Mikhail Tukai <mikhail@imaguru.by>
2017-01-26 23:53:36 +0300

    added selects to form

--- src/js/partials/booking-form.js
+++ src/js/partials/booking-form.js
@@ -5,18 +5,50 @@ if (document.getElementsByClassName('booking-btn').length > 0) {
   var selectField = form.elements.rooms;
 
   function setOption(e) {
-  // берем аттрибут data-room нажатой кнопки и устаенавливаем выбранным в форме
+    // берем аттрибут data-room нажатой кнопки и устаенавливаем выбранным в форме
     var room = e.currentTarget.getAttribute('data-room');
     selectField.value = room;
   }
 
-  for ( var i = 0; i < buttons.length; i++) {
-  // из каждой кнопки берем аттрибут data-room и добавляем options в select
+  for (var i = 0; i < buttons.length; i++) {
+    // из каждой кнопки берем аттрибут data-room и добавляем options в select
     var rooms = buttons[i].getAttribute('data-room');
     var option = new Option(rooms, rooms, false, false);
     selectField.appendChild(option);
-  // вешаем listener на клик
+    // вешаем listener на клик
     buttons[i].addEventListener('click', setOption);
   }
+
+  function getOptions(element) {
+    var selectName = element[0].parentNode.getAttribute('data-select');
+    var insertElement = form.querySelector('select[name=' + selectName + ']');
+    for (var i = 0; i < element.length; i++) {
+      var item = element[i].getAttribute('data-name');
+      var text = element[i].innerHTML;
+      var newOption = new Option(text, item, false, false);
+      newOption.setAttribute('name', item);
+      insertElement.appendChild(newOption);
+    }
+  }
+  var support = document.querySelectorAll('.support li');
+  var promotion = document.querySelectorAll('.promotion li');
+  getOptions(support);
+  getOptions(promotion);
+
+  // выбираем все кнопки с классом catering
+  var cateringButtons = document.querySelectorAll('button.catering');
+  // проходим циклом по кнопкам
+  for (var x = 0; x < cateringButtons.length; x++) {
+    // ищем родителя, потом его первого потомка и его h2.catering
+    var parent = cateringButtons[x].parentNode;
+    var childText = parent.querySelector('.colored-info-box__title h2').innerHTML;
+    var name = cateringButtons[i].getAttribute('data-order');
+    var newOption = new Option(childText, name, false, false);
+    newOption.setAttribute('name', name);
+    form.elements.catering.appendChild(newOption);
+  }
+
+
+
 
 }
