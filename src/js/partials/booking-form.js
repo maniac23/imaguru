d32d2c96299ad11924255787c8c9d32dff33f702
Mikhail Tukai <mikhail@imaguru.by>
2017-01-27 12:40:42 +0300

    added js to make selectors work

--- src/js/partials/booking-form.js
+++ src/js/partials/booking-form.js
@@ -3,12 +3,32 @@ if (document.getElementsByClassName('booking-btn').length > 0) {
   var buttons = document.getElementsByClassName('booking-btn');
   var form = document.getElementById('booking').getElementsByTagName('form')[0];
   var selectField = form.elements.rooms;
+  var support = document.querySelectorAll('.support li');
+  var promotion = document.querySelectorAll('.promotion li');
 
   function setOption(e) {
     // берем аттрибут data-room нажатой кнопки и устаенавливаем выбранным в форме
     var room = e.currentTarget.getAttribute('data-room');
     selectField.value = room;
   }
+
+  function getOptions(element) {
+    var selectName = element[0].parentNode.getAttribute('data-select');
+    var insertElement = form.querySelector('select[name=' + selectName + ']');
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < element.length; i++) {
+      var item = element[i].getAttribute('data-name');
+      var text = element[i].innerHTML;
+      var newOption = new Option(text, item, false, false);
+      newOption.setAttribute('name', item);
+      fragment.appendChild(newOption);
+    }
+
+    insertElement.appendChild(fragment);
+  }
+
+  getOptions(support);
+  getOptions(promotion);
 
   for (var i = 0; i < buttons.length; i++) {
     // из каждой кнопки берем аттрибут data-room и добавляем options в select
@@ -18,22 +38,6 @@ if (document.getElementsByClassName('booking-btn').length > 0) {
     // вешаем listener на клик
     buttons[i].addEventListener('click', setOption);
   }
-
-  function getOptions(element) {
-    var selectName = element[0].parentNode.getAttribute('data-select');
-    var insertElement = form.querySelector('select[name=' + selectName + ']');
-    for (var i = 0; i < element.length; i++) {
-      var item = element[i].getAttribute('data-name');
-      var text = element[i].innerHTML;
-      var newOption = new Option(text, item, false, false);
-      newOption.setAttribute('name', item);
-      insertElement.appendChild(newOption);
-    }
-  }
-  var support = document.querySelectorAll('.support li');
-  var promotion = document.querySelectorAll('.promotion li');
-  getOptions(support);
-  getOptions(promotion);
 
   // выбираем все кнопки с классом catering
   var cateringButtons = document.querySelectorAll('button.catering');
@@ -47,8 +51,4 @@ if (document.getElementsByClassName('booking-btn').length > 0) {
     newOption.setAttribute('name', name);
     form.elements.catering.appendChild(newOption);
   }
-
-
-
-
 }
--- src/js/partials/org-form.js
+++ src/js/partials/org-form.js
@@ -1,34 +1,34 @@
-$('.maintain-paid li').on('click touch', function() {
-var atribute = $(this).attr('data-name');
-var checkBox = $("input[name='"+ atribute + "']");
-var label = $('label[for="'+ atribute +'"]');
-$(this).toggleClass('checked');
-label.toggleClass('checked');
-checkBox.attr("checked", !checkBox.attr("checked"));
+$('.maintain-paid li').on('click touch', function () {
+  var atribute = $(this).attr('data-name');
+  var option = $("option[name='" + atribute + "']");
+  // var label = $('label[for="' + atribute + '"]');
+  $(this).toggleClass('checked');
+  // label.toggleClass('checked');
+  option.attr('selected', !option.attr('selected'));
 });
 
 
-$('.catering').on('click touch', function(e) {
+$('.catering').on('click touch', function (e) {
   e.preventDefault();
   var atribute = $(this).attr('data-order');
-  var checkBox = $("input[name='"+ atribute + "']");
-  var label = $('label[for="'+ atribute +'"]');
+  var option = $("option[name='" + atribute + "']");
+  // var label = $('label[for="' + atribute + '"]');
   $(this).toggleClass('active');
-  label.toggleClass('checked');
-  checkBox.attr("checked", !checkBox.attr("checked"));
+  // label.toggleClass('checked');
+  option.attr('selected', !option.attr('selected'));
 
 });
 
 
-$('input[type="checkbox"]').on('click', function() {
+$('input[type="checkbox"]').on('click', function () {
   var atribute = $(this).attr('id');
-  var label = $('label[for="'+ atribute +'"]');
-  var element = $('.maintain-paid li[data-name="'+ atribute +'"]');
+  var label = $('label[for="' + atribute + '"]');
+  var element = $('.maintain-paid li[data-name="' + atribute + '"]');
   label.toggleClass('checked');
 
-  if($(this).is(':checked')) {
+  if ($(this).is(':checked')) {
     element.addClass('checked');
   } else {
-     element.removeClass('checked');
+    element.removeClass('checked');
   }
 });
