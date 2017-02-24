6d5924db3a7da64307dfbd3cf10f56c47feb194b
Mikhail Tukai <mikhail@imaguru.by>
2017-02-24 18:57:38 +0300

    vacancies page v2

--- src/js/partials/vacancy.js
+++ src/js/partials/vacancy.js
@@ -1,24 +1,28 @@
 $('.company button').on('click', function(){
   var parent = $(this).parent();
   var vacancy = parent.siblings('.vacancy');
-  vacancy.slideToggle(400, function(){
-    if(vacancy.is(':visible')){
-      scrollTo(vacancy);
-    }
-  }).css('display', 'flex');
+  vacancy.slideToggle(
+    // 400, function(){
+  //   if(vacancy.is(':visible')){
+  //     scrollTo(vacancy);
+  //   }
+  // }
+).css('display', 'flex');
 });
 
 
 var fileInput = $('.vacancy__form input[type="file"]');
-console.log(fileInput);
 
 fileInput.each(function() {
   // console.log(index, value, this);
   $(this).on('change', function(){
     var id = $(this).attr('id');
-    console.log(id);
     var label = $(this).parent().siblings('label[for="'+id+'"]');
-    console.log(label);
-    label.toggleClass('uploaded').text(this.files[0].name);
+    if(this.files.length > 0) {
+      label.addClass('uploaded').text(this.files[0].name);
+    } else {
+      label.removeClass('uploaded').text('Загрузить файл');
+    }
+
   });
 })
