c0bb6a50088d67a131e19095b0a2cd011f8837af
Mikhail Tukai <mikhail@imaguru.by>
2017-03-20 14:27:16 +0300

    sliders fixes

--- src/js/partials/pricelist-slider.js
+++ src/js/partials/pricelist-slider.js
@@ -1,9 +1,18 @@
-if ($('.pricelist > ul') && $('.pricelist > ul li').length > 2) {
+if ($('.pricelist > ul') && $('.pricelist>ul>li').length > 4) {
+  console.log($('.pricelist>ul>li').length);
   $('.pricelist > ul').slick({
     responsive: [
       {
         breakpoint: 5000, // такой костыль чтоб на боьших слайдера не было
         settings: 'unslick'
+      },
+      {
+        breakpoint: 1450,
+        settings: {
+          slidesToShow: 4,
+          arrows: true,
+          draggable: true
+        }
       },
       {
         breakpoint: 1220,
